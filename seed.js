678c8e379c9fccaf4aac0dffd79d20e8514c172b
piotras <piotrek.pokora@gmail.com>
2014-11-04 14:37:06 +0100

    Change redirect_url. Keep clear text password

--- seed.js
+++ seed.js
@@ -3,12 +3,12 @@ var models = require('./models');
 
 models.User.create({
   email: 'alex@example.com',
-  hashed_password: '$2a$10$aZB36UooZpL.fAgbQVN/j.pfZVVvkHxEnj7vfkVSqwBOBZbB/IAAK'
+  hashed_password: '$2a$10$aZB36UooZpL.fAgbQVN/j.pfZVVvkHxEnj7vfkVSqwBOBZbB/IAAK' //test
 }, function() {
   models.OAuthClientsModel.create({
     clientId: 'papers3',
     clientSecret: '123',
-    redirectUri: '/oauth/redirect'
+    redirectUri: 'http://localhost:3005/index.html'
   }, function() {
     process.exit();
   });
