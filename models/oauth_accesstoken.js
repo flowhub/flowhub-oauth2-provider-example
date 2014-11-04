7011b6f996ebc373f65bafad633a6b8f1a720bf3
piotras <piotrek.pokora@gmail.com>
2014-11-04 14:31:14 +0100

    userId in oauth_accesstoken is string, so write string instead of the object

--- models/oauth_accesstoken.js
+++ models/oauth_accesstoken.js
@@ -19,7 +19,7 @@ module.exports.getAccessToken = function(bearerToken, callback) {
 module.exports.saveAccessToken = function(token, clientId, expires, userId, callback) {
   var fields = {
     clientId: clientId,
-    userId: userId,
+    userId: userId.id,
     expires: expires
   };
 
